ca81ce29d6259d555933843e194548151f1f15e8
cam rains <csr66@nau.edu>
2019-04-17 14:25:50 -0700

    finished redux2 mini

--- src/components/Reddit/Reddit.js
+++ src/components/Reddit/Reddit.js
@@ -1,24 +1,46 @@
 import React, { Component } from 'react';
 import Card from './../shared/Card/Card';
 import Loading from './../shared/Loading/Loading';
+import {connect} from 'react-redux'
+import {requestArticles} from "../../ducks/redditReducer"
+
 
 class Reddit extends Component {
   constructor(props) {
     super(props);
-    this.state = { articles: [], loading: true }
+    this.state = { }
   }
+
+  componentDidMount(){
+    this.props.requestArticles()
+  }
+
+
+
   render() {
-    const articles = this.state.articles.map((article => <Card key={article.id} article={article} />))
+    const articles = this.props.articles.map((article => <Card 
+      key={article.id} article={article} />))
+      // console.log(articles)
     return (
       <div className='news-container'>
         <img src="./redditLogo.png" alt="" style={styles.logo} />
-        {this.state.loading ? <Loading /> : <div>{articles}</div>}
+        {this.props.loading ? <Loading /> : <div>{articles}</div>}
       </div>
     )
   }
 }
 
-export default Reddit;
+
+
+function mapStateToProps(state) {
+return state.reddit
+}
+
+const mapDispatchToProps= {
+  requestArticles: requestArticles
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Reddit);
 
 
 const styles = {
---
+++ src/store.js
@@ -0,0 +1,18 @@
+import { createStore, applyMiddleware,combineReducers } from "redux";
+import promiseMiddleWare from "redux-promise-middleware";
+import hackerNewsReducer from "./ducks/hackerNewsReducer";
+import mediumReducer from './ducks/mediumReducer'
+import redditReducer from './ducks/redditReducer'
+const rootReducer = combineReducers({
+    hackerNews: hackerNewsReducer,
+    medium: mediumReducer,
+    reddit: redditReducer
+})
+
+
+
+
+export default createStore(
+  rootReducer,
+  applyMiddleware(promiseMiddleWare)
+);
